20b175208746ec34e310fb35231c1ea077868799
Nick Manos <nmanos@eastonrobotics.org>
2022-01-10 23:45:10 -0500

    Fix wrong controller axis

--- src/main/java/frc/tigerlib/command/DifferentialJoystickDrive.java
+++ src/main/java/frc/tigerlib/command/DifferentialJoystickDrive.java
@@ -23,7 +23,7 @@ public class DifferentialJoystickDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveSys.drive(controller.getLeftY(), controller.getRightY());
+    driveSys.drive(controller.getLeftY(), controller.getRightX());
   }
 
   // Returns true when the command should end.
