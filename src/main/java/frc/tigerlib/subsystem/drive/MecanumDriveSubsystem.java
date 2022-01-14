82a263eeac7bb890b19bf41f5e74cbc50ddb510f
Nick Manos <nmanos@eastonrobotics.org>
2022-01-13 23:19:54 -0500

    Write doc comments for MecanumDriveSubsystem

--- src/main/java/frc/tigerlib/subsystem/drive/MecanumDriveSubsystem.java
+++ src/main/java/frc/tigerlib/subsystem/drive/MecanumDriveSubsystem.java
@@ -7,31 +7,77 @@ import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
+/**
+ * A class for easy bootstrapping of Mecanum drive subsystems.
+ *
+ * <p>
+ * Mecanum drives are rectangular with one wheel on each corner. Each wheel has
+ * rollers toed in
+ * 45 degrees toward the front or back. When looking at the wheels from the top,
+ * the roller axles
+ * should form an X across the robot.
+ *
+ * <p>
+ * Drive base diagram:
+ *
+ * <pre>
+ * \\_______/
+ * \\ |   | /
+ *   |   |
+ * /_|___|_\\
+ * /       \\
+ * </pre>
+ */
 public abstract class MecanumDriveSubsystem extends DriveSubsystemBase {
-    protected MotorController frontLeft, rearLeft, frontRight, rearRight;
     protected MecanumDrive drive;
     protected MecanumDriveOdometry odometer;
     private boolean isFieldOriented;
 
+    /** Constructor. */
     protected MecanumDriveSubsystem() {}
 
-    protected void setup(MotorController frontLeft, MotorController rearLeft, MotorController frontRight,
+    /**
+     * Set drive motors and drive kinematics to create {@link #drive}
+     * and {@link #odometer} objects.
+     * 
+     * @param frontLeft  The motor on the front-left corner.
+     * @param rearLeft   The motor on the rear-left corner.
+     * @param frontRight The motor on the front-right corner.
+     * @param rearRight  The motor on the rear-right corner.
+     * @param kinematics The mecanum drive kinematics for your drivetrain.
+     * 
+     * @see {@link MecanumDriveKinematics}
+     */
+    protected void setMotors(MotorController frontLeft, MotorController rearLeft, MotorController frontRight,
             MotorController rearRight, MecanumDriveKinematics kinematics) {
         
         frontRight.setInverted(true);
         rearRight.setInverted(true);
 
-        this.frontLeft = frontLeft;
-        this.frontRight = frontRight;
-        this.rearLeft = rearLeft;
-        this.rearRight = rearRight;
-
         drive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+        drive.setDeadband(0.0);
 
         resetEncoders();
         odometer = new MecanumDriveOdometry(kinematics, gyro.getRotation2d());
+        isFieldOriented = false;
     }
 
+    /**
+     * Drive method for Mecanum platform.
+     *
+     * <p>
+     * Angles are measured clockwise from the positive X axis. The robot's speed is
+     * independent from its angle or rotation rate.
+     * 
+     * @param xSpeed   The robot's speed along the X axis [-1.0..1.0]. Right is
+     *                 positive.
+     * @param ySpeed   The robot's speed along the Y axis [-1.0..1.0]. Forward is
+     *                 positive.
+     * @param rotation The robot's rotation rate around the Z axis [-1.0..1.0].
+     *                 Clockwise is positive.
+     * 
+     * @see {@link #setFieldOriented(boolean)} for field-oriented controls.
+     */
     public void drive(double xSpeed, double ySpeed, double rotation) {
         if (isFieldOriented) {
             drive.driveCartesian(ySpeed, xSpeed, rotation, gyro.getAngle());
@@ -40,10 +86,26 @@ public abstract class MecanumDriveSubsystem extends DriveSubsystemBase {
         }
     }
 
+    /**
+     * When set to true, the robot is "directionless" and movement is
+     * relative to the field rather than the robot using input from the
+     * gyroscope.
+     * 
+     * <p> Defaults to false.
+     * 
+     * @param isFieldOriented Set field-oriented control.
+     */
     protected void setFieldOriented(boolean isFieldOriented) {
         this.isFieldOriented = isFieldOriented;
     }
 
+    /**
+     * Get the wheel speeds of each wheel.
+     * 
+     * <p> Used for odometry calculation.
+     * 
+     * @return {@link MecanumDriveWheelSpeeds}
+     */
     protected abstract MecanumDriveWheelSpeeds getWheelSpeeds();
 
     @Override
