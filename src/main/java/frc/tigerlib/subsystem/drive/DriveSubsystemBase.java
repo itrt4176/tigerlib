80bf385221956c2a47e026c83b8cc3be7ec593c9
Nick Manos <nmanos@eastonrobotics.org>
2022-01-13 22:13:21 -0500

    Write doc comments for DriveSubsystemBase

--- src/main/java/frc/tigerlib/subsystem/drive/DriveSubsystemBase.java
+++ src/main/java/frc/tigerlib/subsystem/drive/DriveSubsystemBase.java
@@ -9,31 +9,64 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * A base for drive subsystems that can be subclassed for easier
+ * bootstrapping.
+ */
 public abstract class DriveSubsystemBase extends SubsystemBase {
   protected Gyro gyro;
   protected Pose2d robotPosition;
   protected Field2d gameField;
 
-  protected DriveSubsystemBase() {}
+  /** Constructor. */
+  protected DriveSubsystemBase() {
+    robotPosition = new Pose2d();
+    gameField = new Field2d();
+  }
 
+  /**
+   * Retrieve current robot position as a {@link Pose2d}.
+   * @return Current robot position
+   */
   public Pose2d getRobotPosition() {
     return robotPosition;
   }
   
+  /**
+   * Set the current robot position.
+   * 
+   * @param pose {@link Pose2d} of current robot position
+   */
   public abstract void setRobotPosition(Pose2d pose);
 
+  /**
+   * Get distance measured by left side encoder.
+   * 
+   * @return distance
+   */
   protected abstract double getLeftDistance();
 
+  /**
+   * Get distance measured by right side encoder.
+   * 
+   * @return distance
+   */
   protected abstract double getRightDistance();
 
+  /**
+   * Resets the current count to zero on the left and right
+   * encoders.
+   */
   protected abstract void resetEncoders();
 
+  /**
+   * Set the gyroscope used by drive subsystem.
+   * 
+   * @param gyro gyroscope
+   */
   protected void setGyro(Gyro gyro) {
     this.gyro = gyro;
-
     gyro.calibrate();
-    robotPosition = new Pose2d();
-    gameField = new Field2d();
   }
 
   
