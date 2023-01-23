8824eac6d4054354f649dbe4f964c5e76e3784fe
Nick Manos <nmanos@eastonrobotics.org>
2023-01-23 13:02:00 -0500

    Refactor XboxControllerIT for 2023 Trigger/Controller updates
    Deprecate all library DPad classes and methods. They are now redundant
    with WPILib

--- src/main/java/frc/tigerlib/command/button/DPadButton.java
+++ src/main/java/frc/tigerlib/command/button/DPadButton.java
@@ -1,11 +1,18 @@
 package frc.tigerlib.command.button;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.tigerlib.XboxControllerIT.DPadDirection;
 
-/** A {@link Button} that gets its state from the d-pad on an {@link XboxController}. */
+/** 
+ * A {@link Button} that gets its state from the d-pad on an {@link XboxController}. 
+ * 
+ * @deprecated Replace with {@link Trigger} and POV methods in {@link CommandXboxController}.
+*/
+@Deprecated(since = "2023", forRemoval = true)
 public class DPadButton extends POVButton {
     
     /**
@@ -13,7 +20,10 @@ public class DPadButton extends POVButton {
      * 
      * @param controller The {@link XboxController} object that has the d-pad
      * @param direction The desired direction on the d-pad (see {@link DPadDirection})
+     * 
+     * @deprecated Replace with {@link Trigger} and POV methods in {@link CommandXboxController}.
      */
+    @Deprecated(since = "2023", forRemoval = true)
     public DPadButton(XboxController controller, DPadDirection direction) {
         super(controller, direction.value);
     }
@@ -22,9 +32,13 @@ public class DPadButton extends POVButton {
      * Checks whether the current direction of the d-pad is the target direction.
      *
      * @return Whether the direction of the d-pad matches the target direction
+     * 
+     * @deprecated Use {@link #getAsBoolean()}.
      */
-    @Override
+    @Deprecated(since = "2023", forRemoval = true)
     public boolean get() {
-        return super.get();
+        return getAsBoolean();
     }
+
+
 }
