1f83aaedad6b581d4b4e879a669b79deb9139c9c
Nick Manos <nmanos@eastonrobotics.org>
2023-01-23 20:52:00 -0500

    Update mecanum drive subsystem/commands for 2023.
    drive(xSpeed, ySpeed, roation) now follows WPILib North-West-Up axis
    conventions.

--- src/main/java/frc/tigerlib/subsystem/drive/MecanumDriveSubsystem.java
+++ src/main/java/frc/tigerlib/subsystem/drive/MecanumDriveSubsystem.java
@@ -3,6 +3,7 @@ package frc.tigerlib.subsystem.drive;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelPositions;
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
@@ -58,7 +59,7 @@ public abstract class MecanumDriveSubsystem extends DriveSubsystemBase {
         drive.setDeadband(0.0);
 
         resetEncoders();
-        odometer = new MecanumDriveOdometry(kinematics, gyro.getRotation2d());
+        odometer = new MecanumDriveOdometry(kinematics, gyro.getRotation2d(), getWheelPositions());
         isFieldOriented = false;
     }
 
@@ -69,18 +70,18 @@ public abstract class MecanumDriveSubsystem extends DriveSubsystemBase {
      * Angles are measured clockwise from the positive X axis. The robot's speed is
      * independent from its angle or rotation rate.
      * 
-     * @param xSpeed   The robot's speed along the X axis [-1.0..1.0]. Right is
+     * @param xSpeed   The robot's speed along the X axis [-1.0..1.0]. Forward is
      *                 positive.
-     * @param ySpeed   The robot's speed along the Y axis [-1.0..1.0]. Forward is
+     * @param ySpeed   The robot's speed along the Y axis [-1.0..1.0]. Left is
      *                 positive.
      * @param rotation The robot's rotation rate around the Z axis [-1.0..1.0].
-     *                 Clockwise is positive.
+     *                 Counterclockwise is positive.
      * 
      * @see #setFieldOriented(boolean)
      */
     public void drive(double xSpeed, double ySpeed, double rotation) {
         if (isFieldOriented) {
-            drive.driveCartesian(xSpeed, ySpeed, rotation, gyro.getAngle());
+            drive.driveCartesian(xSpeed, ySpeed, rotation, gyro.getRotation2d());
         } else {
             drive.driveCartesian(xSpeed, ySpeed, rotation);
         }
@@ -108,16 +109,18 @@ public abstract class MecanumDriveSubsystem extends DriveSubsystemBase {
      */
     protected abstract MecanumDriveWheelSpeeds getWheelSpeeds();
 
+    protected abstract MecanumDriveWheelPositions getWheelPositions();
+
     @Override
     public void setRobotPosition(Pose2d pose) {
         resetEncoders();
-        odometer.resetPosition(pose, gyro.getRotation2d());
+        odometer.resetPosition(gyro.getRotation2d(), getWheelPositions(), pose);
         robotPosition = odometer.getPoseMeters();
     }
 
     @Override
     public void periodic() {
-        robotPosition = odometer.update(gyro.getRotation2d(), getWheelSpeeds());
+        robotPosition = odometer.update(gyro.getRotation2d(), getWheelPositions());
     }
 
     @Override
